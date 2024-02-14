eb91514fba73418458b8b2f71557df65780b84fe
hozumi-asa1192 <us1nu1tksntkr00@gmail.com>
2024-02-14 11:19:18 +0900

    課題mdn変更

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,10 +1,8 @@
 const date = new Date();
 const [month, day, year] = [
-  date.getMonth(),
+  date.getMonth() + 1,
   date.getDate(),
   date.getFullYear(),
 ];
 
-console.log(year + '年')
-console.log(month + '月');
-console.log(day + '日');
+console.log(year + '年' + month + '月' + day + '日') 
